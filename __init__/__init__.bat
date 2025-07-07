554b0677eb793c8a65426542863b7cbc9b5c1c25
andry81 <andry@inbox.ru>
2025-07-07 06:29:23 +0300

    2025.07.07: * changed: *.bat: flag parse global code improvement, added `-+` flag to begin flags scope

--- __init__/__init__.bat
+++ __init__/__init__.bat
@@ -54,7 +54,7 @@ if not defined LOAD_CONFIG_VERBOSE if %INIT_VERBOSE%0 NEQ 0 set LOAD_CONFIG_VERB
 
 if %NO_GEN%0 EQU 0 (
   call "%%CONTOOLS_BUILD_TOOLS_ROOT%%/load_config_dir.bat" %%* -gen_user_config -- "%%CONTOOLS_ADMIN_PROJECT_INPUT_CONFIG_ROOT%%" "%%CONTOOLS_ADMIN_PROJECT_OUTPUT_CONFIG_ROOT%%" || exit /b
-) else call "%%CONTOOLS_BUILD_TOOLS_ROOT%%/load_config_dir.bat" %%* -- "%%CONTOOLS_ADMIN_PROJECT_INPUT_CONFIG_ROOT%%" "%%CONTOOLS_ADMIN_PROJECT_OUTPUT_CONFIG_ROOT%%" || exit /b
+) else call "%%CONTOOLS_BUILD_TOOLS_ROOT%%/load_config_dir.bat" -+ %%* -- "%%CONTOOLS_ADMIN_PROJECT_INPUT_CONFIG_ROOT%%" "%%CONTOOLS_ADMIN_PROJECT_OUTPUT_CONFIG_ROOT%%" || exit /b
 
 if %NO_GEN%0 EQU 0 (
   call "%%CONTOOLS_BUILD_TOOLS_ROOT%%/mkdir_if_notexist.bat" "%%PROJECT_OUTPUT_ROOT%%" || exit /b
